aed76ff28498bee546d461b3a30f399e3bce4bec
Sina Montazeri <accounts@sinamontazeri.com>
2021-01-14 22:31:20 +0330

    add top nav

--- src/components/top-nav/top-nav.tsx
+++ src/components/top-nav/top-nav.tsx
@@ -1,47 +1,49 @@
-import React, { FC } from "react";
-import styled from "@emotion/styled";
-import { connect } from "react-redux";
-import { RootType } from "core/store";
-import Nav from "react-bootstrap/Nav";
+import React, { FC } from 'react'
+import styled from '@emotion/styled'
+import { connect } from 'react-redux'
+import { RootType } from 'core/store'
+import Nav from 'react-bootstrap/Nav'
 
 const StyledNav = styled(Nav)`
   border-bottom: 1px #ddd solid;
   width: 100%;
-  margin:0;
+  margin: 0;
   z-index: 99;
   background-color: #fff;
-  font-family: "IM Fell English", serif;
+  font-family: 'IM Fell English', serif;
   a {
     color: #414141;
     font-size: 1.4rem;
     font-weight: 400;
   }
-`;
+`
 
 const Status: any = styled(Nav.Link)`
-  color: ${(props: any) => (props.status === 0 ? "black" : "")};
+  color: ${(props: any) => (props.status === 0 ? 'black' : '')};
   font-size: 1rem !important;
   margin-top: 0.3rem;
-`;
+`
 type Props = {
-  theme?: any;
-  status: number;
-};
+  theme?: any
+  status: number
+}
 
 const TopNavComponent: FC<Props> = (props) => {
-  const { status } = props;
+  const { status } = props
 
   return (
     <StyledNav>
       <Nav.Item className="ml-2">
-        <Nav.Link href="/home">Howdy, Sina.</Nav.Link>
+        <Nav.Link href="/">Howdy, Sina.</Nav.Link>
       </Nav.Item>
       <Nav.Item className="ml-2 ">
-        <Status status={status}>{status === 0 ? "(Syncing...)" : "(Synced)"}</Status>
+        <Status status={status}>{status === 0 ? '(Saving...)' : '(All good)'}</Status>
       </Nav.Item>
-      
+
       <Nav.Item className="ml-auto">
-        <Nav.Link eventKey="link-2">Account</Nav.Link>
+        <Nav.Link eventKey="account" href="/account">
+          Account
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item className="ml-1">
         <Nav.Link eventKey="disabled" disabled>
@@ -49,13 +51,13 @@ const TopNavComponent: FC<Props> = (props) => {
         </Nav.Link>
       </Nav.Item>
     </StyledNav>
-  );
-};
+  )
+}
 
 const mapStateToProps = (state: RootType) => ({
   status: state.note.status,
-});
+})
 
-const TopNav = connect(mapStateToProps)(TopNavComponent);
+const TopNav = connect(mapStateToProps)(TopNavComponent)
 
-export default TopNav;
+export default TopNav
