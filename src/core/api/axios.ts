fe34dcc41f6ae2992d40e0b94d8010f61626d885
Sina Montazeri <accounts@sinamontazeri.com>
2021-02-27 00:25:49 +0330

    add axios interceptors

--- src/core/api/axios.ts
+++ src/core/api/axios.ts
@@ -1,8 +1,14 @@
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 
 const inDevelopmentOrTesting =
-  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
+  process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
 
-export const mockServer = inDevelopmentOrTesting ? new MockAdapter(axios) : {};
-export default axios;
+export const mockServer = inDevelopmentOrTesting ? new MockAdapter(axios) : {}
+
+axios.interceptors.response.use((res) => {
+  console.log(res.data)
+  return res
+})
+
+export default axios
--- src/core/api/requesters.ts
+++ src/core/api/requesters.ts
@@ -1,17 +1,38 @@
 import axios from 'axios'
 import cookie from 'js-cookie'
 
-export const postRequester = async (route: string, body: any, authenticated: boolean = true) => {
+export const postRequester = async (route: string, body: any) => {
   const token = cookie.get('tk')
-  const headers = {
-    Authorization: `Bearer ${token}`,
-  }
+
+  axios.interceptors.request.use((req) => {
+    // `req` is the Axios request config, so you can modify
+    // the `headers`.
+    req.headers.authorization = `Bearer ${token}`
+    return req
+  })
+
   try {
-    const response = await axios.post(route, body, { headers })
+    const response = await axios.post(route, body)
     console.log('res', response)
   } catch (error) {
     console.log('err', error)
   }
 }
 
-export const getRequester = (route: string, query: string, authenticated: boolean = true) => {}
+export const getRequester = async (route: string, query: string) => {
+  const token = cookie.get('tk')
+
+  axios.interceptors.request.use((req) => {
+    // `req` is the Axios request config, so you can modify
+    // the `headers`.
+    req.headers.authorization = `Bearer ${token}`
+    return req
+  })
+
+  try {
+    const response = await axios.get(`${route}?${query}`)
+    console.log('res', response)
+  } catch (error) {
+    console.log('err', error)
+  }
+}
