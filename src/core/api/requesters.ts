6d84f315299bdf7c5b725eea04d6a00786fc348e
Sina Montazeri <accounts@sinamontazeri.com>
2021-02-28 21:06:41 +0330

    fix api and mocks

--- src/core/api/requesters.ts
+++ src/core/api/requesters.ts
@@ -1,39 +1,52 @@
 import axios from 'axios'
 import cookie from 'js-cookie'
 import { serialize } from 'core/utils'
+import { apiBase as base } from 'core/config'
+import mockNotes from 'core/services/note/mocks'
+import MockAdapter from 'axios-mock-adapter'
+
+axios.interceptors.response.use((res) => {
+  console.log('res log:', res.data)
+  return res
+})
+
+axios.interceptors.request.use((req) => {
+  console.log('req log:', req)
+  return req
+})
+
+const mock = new MockAdapter(axios, { onNoMatch: 'throwException' })
+mockNotes(mock)
 
 export const postRequester = async (route: string, body: any) => {
   const token = cookie.get('tk')
 
   axios.interceptors.request.use((req) => {
-    // `req` is the Axios request config, so you can modify
-    // the `headers`.
     req.headers.authorization = `Bearer ${token}`
     return req
   })
 
   try {
     const response = await axios.post(route, body)
-    console.log('res', response)
+    return response
   } catch (error) {
     console.log('err', error)
   }
 }
 
 export const getRequester = async (route: string, query?: { [key: string]: any }) => {
-  const serializedQuery = query ? serialize(query) : ''
+  const serializedQuery = query ? '?' + serialize(query) : ''
   const token = cookie.get('tk')
 
   axios.interceptors.request.use((req) => {
-    // `req` is the Axios request config, so you can modify
-    // the `headers`.
     req.headers.authorization = `Bearer ${token}`
     return req
   })
 
   try {
-    const response = await axios.get(`${route}?${serializedQuery}`)
-    console.log('res', response)
+    const url = `${base}${route}${serializedQuery}`
+    const response = await axios.get(url)
+    return response
   } catch (error) {
     console.log('err', error)
   }
