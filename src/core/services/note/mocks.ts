59bfae1f728e82f55f522a598f61e9ea63d3415b
Sina Montazeri <accounts@sinamontazeri.com>
2021-03-04 18:53:50 +0330

    create note list logic for mocks

--- src/core/services/note/mocks.ts
+++ src/core/services/note/mocks.ts
@@ -1,15 +1,35 @@
-import axios from 'axios'
-import MockAdapter from 'axios-mock-adapter'
+import MockAdapter from 'axios-mock-adapter/types'
+import { apiBase } from 'core/config'
 import { emptyNotePlaceHolder } from 'core/utils'
-;(() => {
-  let mockNotes
-  if (process.env.REACT_APP_MOCK_NOTE_RESPONSES) {
-    mockNotes = new MockAdapter(axios)
-  }
+import { format } from 'date-fns'
+import { v4 as uuidv4 } from 'uuid'
+
+export default (mockNotes: MockAdapter) => {
+  if (!process.env.REACT_APP_MOCK_NOTE_RESPONSES) return
 
   if (!mockNotes) return
 
-  const notesList: String[] = Array(5).map((note) => emptyNotePlaceHolder)
+  let lastServedIndex = 0
+  const noteCount: Number = 100
 
-  mockNotes.onGet('/note/list').reply(200, notesList)
-})()
+  const notesList = (limit: number): Note[] => {
+    if (lastServedIndex >= noteCount) return []
+    lastServedIndex += limit
+    const mockedList = [...Array(limit)].map(() => {
+      const key = uuidv4()
+      const date = format(new Date(), 'MMM dd yyyy')
+
+      return {
+        body: emptyNotePlaceHolder,
+        key,
+        date,
+      }
+    })
+    return mockedList
+  }
+
+  mockNotes.onGet(/\/note\/list/).reply(() => {
+    return [200, { notesList: notesList(10) }]
+  })
+  mockNotes.onGet(apiBase + '/note/count').reply(200, { noteCount })
+}
